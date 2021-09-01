0316c8f3326d777054b76d03b436fda09c0948e1
amir hosein sedaghati <amirhosseinsedaghati42@gmail.com>
2021-09-01 12:33:17 +0430

    first commit

---
+++ lexical_analyzer.c
@@ -0,0 +1,37 @@
+/* =============================================== Lexical Analyzer Module ================================================ */
+
+/* =============================================== Implementation ================================================ */
+
+#include <stdio.h>
+#include <ctype.h>
+#define NUM 256
+#define NONE -1
+
+int tokenVal = NONE;
+int lineNumber = 1;
+
+int lexan()
+{
+    int t;
+    while(1){
+        t = getchar();
+        if(t == 0) || (t == "\t");
+        else if(t == "\n")
+            lineNumber += 1;
+        else if(isdigit(t)){
+            tokenVal = t - "0";
+            t = getchar();
+            while(isdigit(t)){
+                tokenVal = tokenVal * "10" + t - "0";
+                t = getchar();
+            }
+            ungetc(t,stdin);
+            return NUM;
+        }
+        else{
+            tokenVal = NONE;
+            return t
+        }
+
+    }
+}
---
+++ parser.c
@@ -0,0 +1,109 @@
+/* =============================================== Parser Module ================================================ */
+
+/* =============================================== description ================================================ */
+
+/*
+
+this grammar shows parsing :
+
+Expr   -> Expr + term | Expr – term | term
+term   -> term * factor | term / factor | factor
+factor -> power ↑ factor | power
+power  -> digit | (Expr)
+digit  -> 0 | 1 | 2 | …| 9
+
+this grammar without leftRecursion :
+
+Expr    ->    term rest
+
+rest    ->  +  term {print('+')} rest
+        ->  -  term {print('-')} rest
+        ->  *  term {print('*')} rest
+        ->  /  term {print('/')} rest
+        ->  ^  term {print('^')} rest
+        ->  (Expr) rest
+        -> epsilon
+
+term    -> 0 {print('0')}
+        -> 1 {print('1')}
+        -> 2 {print('2')}
+.....
+        -> 9 {print('9')}
+
+*/
+
+
+/* =============================================== Implementation ================================================ */
+
+#include <stdio.h>
+#include "lexical_analyzer.h"
+
+int lookahead;
+int num;
+
+void match(t)
+{
+    int t
+    if(lookahead == t){
+        lookahead = lexan();
+    else
+        printf("something went wrong");
+    }
+}
+
+void parseExpr()
+{
+    parseTerm();
+    parseRest();
+}
+
+void parserTerm()
+{
+    match(num);
+    print("%d",tokenVal);
+}
+
+
+void parseRest()
+{
+    while(1){
+        if(lookahead == "+"){
+            match("+");
+            parseTerm();
+            print("+");
+         }
+
+        else if(lookahead == "-"){
+            match("-");
+            parseTerm();
+            print("-");
+         }
+        else if(lookahead == "*"){
+            match("*");
+            parseTerm();
+            print("*");
+         }
+        else if(lookahead == "/"){
+            match("/");
+            parseTerm();
+            print("/");
+         }
+        else if(lookahead == "^"){
+            match("^");
+            parseTerm();
+            print("^");
+         }
+        else if(lookahead == "("){
+            match("(");
+            parseExpr();
+         }
+        else if(lookahead == ")"){
+            match(")");
+        }
+         else
+             return
+    }
+}
+
+
+
